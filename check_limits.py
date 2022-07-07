05d4d096877e1a7ccd6e9ab6adf7b1c6c3104712
Naik Punith <pnu5kor@bosch.com>
2022-07-07 23:01:07 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -22,3 +22,5 @@ def battery_is_ok(temperature, soc, charge_rate):
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
   assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(0, 20, 0.8) is True)
+  assert(battery_is_ok(46, 81, 0.9) is False)
